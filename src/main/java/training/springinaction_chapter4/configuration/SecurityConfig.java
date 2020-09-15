49f8d426daa7f09379945babbffff480e411b002
Piotr Turlinski <piotrturlinski2@gmail.com>
2020-09-15 20:10:38 +0200

    InMemoryAuthentication changed to JDBC

--- src/main/java/training/springinaction_chapter4/configuration/SecurityConfig.java
+++ src/main/java/training/springinaction_chapter4/configuration/SecurityConfig.java
@@ -1,29 +1,35 @@
 package training.springinaction_chapter4.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
                 .withUser("buzz")
-                    .password("infinity")
-                    .authorities("ROLE_USER")
-                .and()
-                .withUser("woody")
-                    .password("bullseye")
-                    .authorities("ROLE_USER").and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                    .password(passwordEncoder().encode("infinity"))
+                    .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
